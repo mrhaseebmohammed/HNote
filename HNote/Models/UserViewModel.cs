ab5089ab7a453f3d67e978277aff8cea8a2069b5
pineapplechunks <mrhaseebmohammed@gmail.com>
2016-09-19 21:35:07 -0500

    Initial Commit

---
+++ HNote/Models/UserViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HNote.Entities;
+
+namespace HNote.Models
+{
+    public class UserViewModel
+    {
+        public string Username { get; set; }
+    }
+}
